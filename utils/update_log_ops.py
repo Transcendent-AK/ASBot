12b406584389d3462590fa425f17fcede3ca6f46
denunciated <kahzukie@pm.me>
2025-09-03 15:47:15 -0300

    feat: comment watchlist part (for now) and hardcode the admin names so the list on the sheet can find the values

--- utils/update_log_ops.py
+++ utils/update_log_ops.py
@@ -10,6 +10,38 @@ def log_update(user_name, change_description):
         user_name (str): Name of the user who made the change
         change_description (str): Description of the change made
     """
+
+    #HardCoded admin names
+    if user_name == 'kahzukie':
+        user_name = 'Kahz'
+
+    if user_name == '.onlyman':
+        user_name = 'Beaako'
+
+    if user_name == 'gds_':
+        user_name = 'Gds'
+
+    if user_name == 'wpmz':
+        user_name = 'Exdel'
+
+    if user_name == 'skar_8685':
+        user_name = 'Skar'
+
+    if user_name == 'reginaphalange9799':
+        user_name = 'Luna'
+
+    if user_name == 'kitsuneblaze0592':
+        user_name = 'Kitsu'
+
+    if user_name == 'night.flower':
+        user_name = 'Nyx'
+
+    if user_name == 'kyzeyy':
+        user_name = 'Kyzey'
+
+    if user_name == 'voyagerloaf':
+        user_name = 'lLof'
+
     sheet = get_sheet('Update Sheet')
     date_str = datetime.now().strftime('%Y/%m/%d')
     sheet.append_row([date_str, user_name, change_description])
